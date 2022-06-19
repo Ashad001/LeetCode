a3b7d21f6f3e89902cca170d407e55b21ff474ba
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-06-19 18:36:38 +0500

    Time: 46 ms (25.83%), Space: 19.4 MB (13.79%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+        for(int j = 0; j < n; j++)
+        {
+            if(nums[j] != 0)
+            {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        for(int j = i; j < nums.size(); j++)
+        {
+            nums[j] = 0;
+        }
+    }
+};
