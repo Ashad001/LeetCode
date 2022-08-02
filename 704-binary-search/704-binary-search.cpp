db4f286be2e5e105fa392a0e396c308211cef5bb
Ashad <93534298+Ashad001@users.noreply.github.com>
2022-08-02 21:25:54 +0500

    Time: 47 ms (77.64%), Space: 27.6 MB (12.40%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            if(target < nums[mid])
-                end = mid - 1;
-            else if(target > nums[mid])
-                start = mid + 1;
-            else 
-                return mid;
-        }
-        return -1;
+        return Recursion(nums,target, 0, nums.size());
+    }
+    int Recursion(vector<int> &nums, int target, int start, int end)
+    {
+        int mid = start + (end - start) / 2;
+        if(start >= end)
+            return -1;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            return Recursion(nums,target, mid + 1, end);
+        else 
+            return Recursion(nums,target, start, end - 1);
     }
 };
