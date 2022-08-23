e425e266ecd7a7e5fe7d66569759780ed87fceac
Ashad <93534298+Ashad001@users.noreply.github.com>
2022-08-23 20:19:27 +0500

    Time: 27 ms (37.67%), Space: 5.9 MB (31.85%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+        int rev = 0;
+        while(x > rev)
+        {
+            rev = rev* 10 + (x % 10);
+            x /= 10;
+        }
+       
+        return (x == rev / 10 || x == rev) ;
+    }
+};
