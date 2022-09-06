e9de8ce8787a388c37225e04c95b66d855036d29
Ashad <93534298+Ashad001@users.noreply.github.com>
2022-09-06 20:02:34 +0500

    Time: 74 ms (16.76%), Space: 45.7 MB (13.78%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+   
     bool Check(vector<string> arr, int row, int col)
     {
         int i{0}, j{0};
@@ -26,31 +27,13 @@ public:
         }
         return true;
     }
-    bool countQueens(vector<string> arr)
-    {
-        int count = 0;
-        for (int i = 0; i < arr.size(); i++)
-        {
-            for (int j = 0; j < arr.size(); j++)
-            {
-                if (arr[i][j] == 'Q')
-                {
-                    count++;
-                }
-            }
-        }
-        if (count >= arr.size())
-        {
-            return true;
-        }
-        return false;
-    }
     
-    vector<vector<string>> NQueens(vector<string> &arr, int col, vector<vector<string>> &ans)
+    void NQueens(vector<string> &arr, int col, vector<vector<string>> &ans)
     {
         if (col >= arr.size())
         {
-            return ans;
+            ans.push_back(arr);
+            return;
         }
     
         for (int i = 0; i < arr.size(); i++)
@@ -58,15 +41,10 @@ public:
             if (Check(arr, i, col))
             {
                 arr[i][col] = 'Q';
-                if (countQueens(arr))
-                {
-                    ans.push_back(arr);
-                }
                 NQueens(arr, col + 1, ans);
                 arr[i][col] = '.';
             }
         }
-        return ans;
     }
     
     vector<vector<string>> solveNQueens(int n)
@@ -74,6 +52,90 @@ public:
         string str(n, '.');
         vector<string> arr(n, str);
         vector<vector<string>> ans;
-        return NQueens(arr, 0, ans);
+        NQueens(arr, 0, ans);
+        return ans;
     }
+        
+        
+/*
+        Runtime: 1345 ms, faster than 5.08% of C++ online submissions for N-Queens.
+        Memory Usage: 463.9 MB, less than 5.81% of C++ online submissions for N-Queens.
+    
+*/
+//     bool Check(vector<string> arr, int row, int col)
+//     {
+//         int i{0}, j{0};
+//         for (i = 0; i < col; i++)
+//         {
+//             if (arr[row][i] == 'Q')
+//             {
+//                 return false;
+//             }
+//         }
+//         for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+//         {
+//             if (arr[i][j] == 'Q')
+//             {
+//                 return false;
+//             }
+//         }
+//         for (i = row, j = col; i < arr.size() && j >= 0; i++, j--)
+//         {
+//             if (arr[i][j] == 'Q')
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     bool countQueens(vector<string> arr)
+//     {
+//         int count = 0;
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+//             for (int j = 0; j < arr.size(); j++)
+//             {
+//                 if (arr[i][j] == 'Q')
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+//         if (count >= arr.size())
+//         {
+//             return true;
+//         }
+//         return false;
+//     }
+    
+//     vector<vector<string>> NQueens(vector<string> &arr, int col, vector<vector<string>> &ans)
+//     {
+//         if (col >= arr.size())
+//         {
+//             return ans;
+//         }
+    
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+//             if (Check(arr, i, col))
+//             {
+//                 arr[i][col] = 'Q';
+//                 if (countQueens(arr))
+//                 {
+//                     ans.push_back(arr);
+//                 }
+//                 NQueens(arr, col + 1, ans);
+//                 arr[i][col] = '.';
+//             }
+//         }
+//         return ans;
+//     }
+    
+//     vector<vector<string>> solveNQueens(int n)
+//     {
+//         string str(n, '.');
+//         vector<string> arr(n, str);
+//         vector<vector<string>> ans;
+//         return NQueens(arr, 0, ans);
+//     }
 };
