19040097ef5c5c3a52cb66205564f380d8c4dc96
Ashad <93534298+Ashad001@users.noreply.github.com>
2023-08-18 14:35:35 +0500

    Time: 27 ms (94.77%), Space: 7.1 MB (30.16%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+    start := 0
+    end := len(nums)
+    for start < end {
+        mid := (start + end) / 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] < target {
+            start = mid + 1
+        } else {
+            end = mid 
+        }
+    }
+    return -1
+}
