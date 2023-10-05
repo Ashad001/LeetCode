e46b79ae03107825021bd7a52c2cefc92fdf4b6c
Ashad <93534298+Ashad001@users.noreply.github.com>
2023-10-05 23:02:38 +0500

    Time: 1 ms (83.45%), Space: 2 MB (96.09%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -0,0 +1,10 @@
+func isSubsequence(s string, t string) bool {
+    i, j := 0, 0
+    for i < len(s) && j < len(t) {
+        if (s[i] == t[j]) {
+            i++
+        }
+        j++
+    }
+    return i == len(s)
+}
