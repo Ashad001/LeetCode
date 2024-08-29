9d564fc1ce7f89b7929244dd9dc142de1bdb9959
Ashad <93534298+Ashad001@users.noreply.github.com>
2024-08-29 23:07:10 +0500

    Time: 16 ms (20.85%), Space: 24.9 MB (26.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> leftMax(n, 0);
-        vector<int> rightMax(n, 0);
-        int left = 0;
-        int right = 0;
         
-        for (auto i = 1, j = n - 2; i < n, j > 0; i++, j--) {
-            if (height[i - 1] > left) {
-                left = height[i - 1];
-            }   
-            leftMax[i] = left;
-            if (height[j + 1] > right) {
-                right = height[j + 1];
-            }
-            rightMax[j] = right;
-        }
+        int left = 0;
+        int right = n - 1;
+        int leftMax = height[0];
+        int rightMax = height[n - 1];
         
         int count = 0;
-        for (auto i = 0; i < n; i++) {
-            int trap = (min(leftMax[i], rightMax[i]) - height[i]);
-            if (trap > 0) {
-                count+=trap;
-            }
+        
+        while (right > left) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = max(leftMax, height[left]);
+                count += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = max(rightMax, height[right]);
+                count += rightMax - height[right];
+            }          
         }
         return count;
     }
+    
     int min(int a, int b) {
         return a < b ? a : b;
+    }
+    
+    int max(int a, int b) {
+        return a > b ? a : b;
     }
 };
